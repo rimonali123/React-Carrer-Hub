0c4d874e675fb98daffe4c4f7c4d924f92435ffe
rimonali123 <rimonali450@gmail.com>
2024-03-23 00:43:51 +0600

    show details btn added

--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -9,7 +9,17 @@ const JobDetails = () => {
     console.log(job)
     return (
         <div>
-           
+            <h2>Job Details : </h2>
+            <div className="grid grid-col lg:grid-cols-4 gap-6">
+                <div className="border col-span-3 px-2">
+                    <h2 className=" txt-4xl">data coming here</h2>
+                </div>
+                <div className="border col-span-1 px-2">
+                    <h2 className=" txt-4xl">Some Things</h2>
+                    <button className="btn btn-primary w-full ">Apply Now</button>
+                </div>
+
+            </div>
         </div>
     );
 };
